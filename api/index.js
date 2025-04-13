253d8556791f8a5d73bd2dee360038c719d59d14
arryaanjain <jainarryaan@gmail.com>
2025-04-13 17:33:08 +0530

    Initial commit with working JWT for login and delete of users

---
+++ api/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+app.use(express.json());
+
+const users = [
+    {
+        id:"1",
+        username:"John",
+        password:"John123",
+        isAdmin:true,
+    },
+    {
+        id:"2",
+        username:"Jane",
+        password:"Jane123",
+        isAdmin:false,
+    },
+    {
+        id:"3",
+        username:"Jessie",
+        password:"Jess123",
+        isAdmin:false,
+    },
+];
+
+app.post("/api/login", (req,res) => {
+    const {username, password} = req.body;
+    const user = users.find(u=> {
+        return u.username === username && u.password === password;
+    });
+    if (user) {
+        //Generate an access token
+        const accessToken = jwt.sign({id:user.id, isAdmin:user.isAdmin }, "mySecretKey");
+        res.json({
+            username: user.username,
+            isAdmin: user.isAdmin,
+            accessToken: accessToken,
+        });
+    } else {
+        res.status(401).json({message: "Invalid credentials"});
+    }
+})
+
+
+const verify = (req,res,next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, "mySecretKey", (err,user) => {
+            if(err) {
+                return res.status(403).json("Token is invalid");
+            }
+
+            req.user = user;
+            next();
+        })
+    } else {
+        res.status(401).json("You aint authenticated");
+    }
+}
+
+app.delete("/api/users/:userId", verify, (req,res) => {
+    //middleware: using the verify function to check if the user is authenticated
+    if(req.user.id === req.params.userId || req.user.isAdmin) {
+        res.status(200).json("User deleted");
+    } else {
+        res.status(403).json("Not allowed to delete this user");
+    }
+})
+
+app.listen(5030, ()=> console.log("Server is running on port 5030"));
